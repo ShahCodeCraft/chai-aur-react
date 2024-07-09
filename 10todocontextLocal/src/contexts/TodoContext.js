d7a21c344b40b676b8fcb4e58b1ef1eaac0e2b04
ShahCodeCraft <ershahalam22@gmail.com>
2024-07-09 18:44:39 +0530

    Todo-Context-LocalStorage Project

--- 10todocontextLocal/src/contexts/TodoContext.js
+++ 10todocontextLocal/src/contexts/TodoContext.js
@@ -1,13 +1,17 @@
 import { createContext, useContext } from "react";
 
 export const TodoContext = createContext({
-    todo: [
+    todos: [
         {
             id: 1,
             todo: "Todo msg",
             completed: false,
-        } 
-    ]
+        }, {} 
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id,todo) =>{},
+    deleteTodo: (id)=>{},
+    toggleComplete: (id) => {}
 })
 
 export const useTodo = () =>{
